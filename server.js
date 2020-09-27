0e135a447ce8ad4ff2d87f0b31f913d2ddbd699e
Braeden <bcraig@urbancoolab.com>
2020-09-27 16:39:36 -0400

    fixed deployment issues in server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const connectDB = require('./config/db')
-const path = require('path')
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -8,26 +8,23 @@ const app = express();
 connectDB();
 
 // Init Middleware, where body parser would happen but is included in express now
-app.use(express.json({ extended: false }))
-
-// app.get('/', (req, res) => res.send('API Running'));
-// We could go app.gets for all in routes/api but going to get messy so we break it up into different components
+app.use(express.json({ extended: false }));
 
 // Define routes here
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/auth', require('./routes/api/auth'))
-app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 // // Serve static assets  in production
-// if(process.env.NODE_ENV === 'production') {
-//   // set static folder
-//   app.use(express.static('client/build'));
+if(process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
 
-//   app.get('*', (req, res) => { 
-//     res.sendFile(path.resolve()(__dirname, 'client', ' build', 'index.html'))
-//   })
-// }
+  app.get('*', (req, res) => { 
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 
 const PORT = process.env.PORT || 5000;
